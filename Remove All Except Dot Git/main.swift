e87093584f00ab529743d51b5841906968fb9451
Daniel <d108@ikiApps.com>
2016-11-16 10:21:52 -1000

    Updated to Swift 3. Added initial argument checking.
    Fixed a problem counting characters.

--- Remove All Except Dot Git/main.swift
+++ Remove All Except Dot Git/main.swift
@@ -23,58 +23,70 @@
 import Foundation
 import Darwin
 
-/**
-
- Remove all directories and files for a given path except those for Git.
- These include .git and .gitignore.
-
- This is useful for maintaining a repository that is dynamically generated
- such as with a Jekyll site.
-
- The use case is where you want to ensure that no previous files from a previous Jekyll build
- are lying around in your directory that you use for publishing your blog.
-
- */
-
-let args = [String](Process.arguments)
-let fm = NSFileManager.defaultManager()
-let dest = args[1]
-var isDir = ObjCBool(false)
-let ignoreItems = [".git", ".gitignore"]
+/// Remove all directories and files for a given path except those for Git.
+/// These include .git and .gitignore.
+///
+/// This is useful for maintaining a repository that is dynamically generated
+/// such as with a Jekyll site.
+///
+/// The use case is where you want to ensure that no previous files from a previous Jekyll build
+/// are lying around in your directory that you use for publishing your blog.
 
 /// Handle exit.
-func reportExit() {
+func reportExit()
+{
     print("Exiting without deleting.")
 }
 
 /// Obtain input from the console.
-func input() -> String {
-    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+func input() -> String
+{
+    let keyboard = FileHandle.standardInput
     let inputData = keyboard.availableData
-    return NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
+    return String(data: inputData,
+                  encoding: String.Encoding.utf8)!;
 }
+
+let args = [String](CommandLine.arguments)
+
+guard args.count == 2 else {
+    print("Usage: removeAllExceptDotGit.swift ${DIRECTORY_NAME}")
+    reportExit()
+    exit(0);
+}
+
+// TODO: Validate directory.
+
+let fm = FileManager.default
+let dest = args[1]
+var isDir = ObjCBool(false)
+let ignoreItems = [".git", ".gitignore"]
 
 print("Preparing to DELETE all files except those related to Git.\n")
 print("Checking if the destination directory exists...")
 
-if fm.fileExistsAtPath(dest, isDirectory: &isDir) {
+if fm.fileExists(atPath: dest,
+                 isDirectory: &isDir)
+{
     print("\(dest) exists.\n")
 }
 
-guard isDir else {
-    exit(255)
+guard isDir.boolValue else {
+    exit(255);
 }
 
 print("Should I proceed to delete content in \(dest), enter \"yes\" to proceed?")
 var userInput = input()
 
-guard userInput.characters.count != 3 else {
-    print("Character count was not 3")
+guard userInput.characters.count == 4 else {
+    print("Invalid input. Character count was not 4.")
     reportExit()
     exit(0);
 }
 
-let firstChars = userInput[userInput.startIndex.advancedBy(0)...userInput.startIndex.advancedBy(2)]
+let firstChars = userInput[userInput.characters.index(userInput.startIndex,
+                                                      offsetBy: 0)...userInput.characters.index(userInput.startIndex,
+                                                                                                offsetBy: 2)]
 
 guard firstChars == "yes" else {
     print("Did not receive 'yes'")
@@ -83,8 +95,9 @@ guard firstChars == "yes" else {
 }
 
 var contents = Array<String>()
+
 do {
-    contents = try fm.contentsOfDirectoryAtPath(dest)
+    contents = try fm.contentsOfDirectory(atPath: dest)
 } catch let error as NSError {
     print("error \(error)")
 }
@@ -93,10 +106,9 @@ for item in contents {
     if !ignoreItems.contains(item) {
         print("removing \(item)")
         do {
-            try fm.removeItemAtPath("\(dest)/\(item)")
+            try fm.removeItem(atPath: "\(dest)/\(item)")
         } catch let error {
             print("error \(error)")
         }
     }
 }
-
